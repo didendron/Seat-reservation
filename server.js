e5ef87d2b93b94fd62b24f419cb49d6f3f6ab4aa
didendron <ruszkow11@wp.pl>
2021-06-22 13:50:36 +0200

    changes to deploy5

--- server.js
+++ server.js
@@ -4,17 +4,10 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 5000;
 
-const path = require('path');
-const express = require('express');
-app.use('/db', middlewares, router);
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 
-//server.use(middlewares);
-//server.use(router);
+
+server.use(middlewares);
+server.use(router);
 
 server.listen(port);
