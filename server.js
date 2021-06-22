ccdb2b6a89734bb8c7ae3e9be4affc1770643bf5
didendron <ruszkow11@wp.pl>
2021-06-22 15:51:13 +0200

    changes to deploy5

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const express = require('express');
 app.use('/db', middlewares, router);
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
